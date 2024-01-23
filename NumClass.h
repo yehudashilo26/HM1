c23226522d8551102c636927a8cc2bc336049e2b
yehudashilo26 <157530536+yehudashilo26@users.noreply.github.com>
2024-01-23 22:57:09 +0200

    commit1

---
+++ NumClass.h
@@ -0,0 +1,16 @@
+
+#ifndef _NUMCLASS_H_
+#define _NUMCLASS_H_
+
+int revP(int, int);
+int checkArm(int ,int);
+// /*will return if a number is a armstrong*/
+int isArmstrong(int);
+// /*will return if a number is a palindrome*/
+int isPalindrome(int);
+// /*will return if a number is prime*/
+int isPrime(int);
+// /*will return if a number is strong*/
+int isStrong(int);
+
+#endif
